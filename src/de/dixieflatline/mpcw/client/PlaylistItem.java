59f54261d92e85b309ab52ea8098d8897ce94fe2
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2018-09-25 15:41:15 +0200

    PlaylistItem: equals() && hashCode() implementations

--- src/de/dixieflatline/mpcw/client/PlaylistItem.java
+++ src/de/dixieflatline/mpcw/client/PlaylistItem.java
@@ -20,11 +20,13 @@ public class PlaylistItem
 {
 	private final String artist;
 	private final String title;
+	private final int hashCode;
 	
 	public PlaylistItem(String artist, String title)
 	{
 		this.artist = artist;
 		this.title = title;
+		hashCode = calculateHashcode();
 	}
 
 	public String getTitle()
@@ -35,5 +37,69 @@ public class PlaylistItem
 	public String getArtist()
 	{
 		return artist;
+	}
+
+	int calculateHashcode()
+	{
+		final int prime = 31;
+		int result = 1;
+		
+		result = prime * result + ((artist == null) ? 0 : artist.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		
+		return result;
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		return hashCode;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if(this == obj)
+		{
+			return true;
+		}
+		
+		if(obj == null)
+		{
+			return false;
+		}
+		
+		if(getClass() != obj.getClass())
+		{
+			return false;
+		}
+		
+		PlaylistItem other = (PlaylistItem) obj;
+
+		if(artist == null)
+		{
+			if (other.artist != null)
+			{
+				return false;
+			}
+		}
+		else if(!artist.equals(other.artist))
+		{
+			return false;
+		}
+		
+		if(title == null)
+		{
+			if(other.title != null)
+			{
+				return false;
+			}
+		}
+		else if(!title.equals(other.title))
+		{
+			return false;
+		}
+
+		return true;
 	}
 }
